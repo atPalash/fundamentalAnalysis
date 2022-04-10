75ad12a381294c4e3e2f2934dde9daa980fb52fd
palashTUT <mpalash.halder@gmail.com>
2022-04-10 12:25:34 +0300

    Features update - adding dummy commands to test user input - adding clear messages

--- orchestrator/route_methods.py
+++ orchestrator/route_methods.py
@@ -1,17 +1,50 @@
-def hello(data):
-    res = f"Hello from discord you said {data}"
-    return res
-
-
-def add(data):
-    val = data.split(",")
+def add(*args):
+    """
+    Add two number. input argument is split into separate numbers using ","
+    e.g.:
+    add: 10, 20
+    Keyword arguments:
+    data -- numbers separated by ","
+    """
+    val = args[0].split(",")
     val1 = float(val[0])
     val2 = float(val[1])
     res = val1 + val2
     return str(res)
 
 
-route_methods = {
-    "hello":    hello,
-    "add":      add,
-    }
+def commands(*args):
+    """
+    List all methods which can be queried from Discord.
+    e.g.:
+    commands: all -> list all command api.
+    commands: <command> -> shows spi for specified command.
+    Keyword arguments:
+    data -- numbers separated by ","
+    """
+    res = ""
+    if args[0] == "all":
+        for method, func in route_methods.items():
+            res += f"{method}:{func.__doc__}\n"
+    else:
+        if route_methods.get(args[0]) is not None:
+            res = route_methods[args[0]].__doc__
+        else:
+            res = "This method is not available, check all methods available commands: all"
+
+    return res
+
+
+def hello(*args):
+    """
+    Says hello to user.
+    e.g.
+    hello: user
+    Keyword arguments:
+    data -- any user input
+    """
+    res = f"Hello from discord you said {args[0]}"
+    return res
+
+
+route_methods = dict(add=add, commands=commands, hello=hello)

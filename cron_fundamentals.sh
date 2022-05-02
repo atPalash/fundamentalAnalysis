768365d342e004dbb4aeb94643ba4200986dcd40
palashTUT <mpalash.halder@gmail.com>
2022-05-03 01:55:09 +0530

    updating cron job for fetching bi-weekly fundamental data

--- cron_fundamentals.sh
+++ cron_fundamentals.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cd "$(dirname "$1")" || exit
+cd "$(dirname "$0")" || exit
 CWD="$(pwd)"
-
 echo $CWD downloading fundamentals and converting to excel
 $CWD/.venv/bin/python $CWD/stockFundamentals/key_stats_to_stock_select.py
